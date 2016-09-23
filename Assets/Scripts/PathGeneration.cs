ccca5cd0377ef696b0bd2b61519f1bc149a72625
Goos van den Bekerom <g.vandenbekerom@student.fontys.nl>
2016-09-23 08:49:04 +0200

    Added multiple platforms for random world generation

--- Assets/Scripts/PathGeneration.cs
+++ Assets/Scripts/PathGeneration.cs
@@ -1,67 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class PathGeneration : MonoBehaviour
     {
-        public GameObject Platform;
+        public GameObject[] Platforms;
         public int RenderDistance;
 
         private float _platformLength;
-        private Transform _firstPlatform;
         private Transform _lastPlatform;
-
-        public Transform Player;
 
         void Start()
         {
-            _platformLength = Platform.transform.localScale.z;
+            _platformLength = Platforms[0].transform.localScale.z;
+
+            SpawnPlatform(false, -_platformLength); // so there is always at least one platform behind camera
 
             for (var i = 0; i < RenderDistance; i++)
             {
-                SpawnPlatform(false, i*_platformLength);
+                SpawnPlatform(false, i * _platformLength);
             }
-
-            _firstPlatform = GameManager.Instance.Platforms.Peek().transform;
+            
         }
-
-        void FixedUpdate()
-        {
-            /*// move platforms (this gameobject is parent to all platforms)
-            transform.Translate(0, 0, -GameManager.Instance.MovementSpeed);
-
-            if (GameManager.Instance.Platforms.Peek().transform.position.z < -_platformLength)
-            {
-                SpawnPlatform(_lastPlatform.transform.position.z + _platformLength);
-
-                var oldPlatform = GameManager.Instance.Platforms.Dequeue();
-                Destroy(oldPlatform);
-            }*/
-            // TODO: Levels genereren voor de speler, object plaatsen waar speler mee botst om te weten
-            // TODO: dat de speler op een nieuw platform is....
-        }
-
+        
         /// <summary>
         /// Spawn a new platform at the end of the world.
         /// </summary>
+        /// <param name="removeOld"> First platform in world queue will be removed when true </param>
+        /// <param name="zPos"> z position to spawn the new platform at </param>
         /// <returns> The newly instantiated platform </returns>
         public GameObject SpawnPlatform(bool removeOld, float zPos = -1)
         {
-            var pos = Platform.transform.position;
+            var world = GameManager.Instance.ActivePlatforms;
+            var platformNumber = (world.Count < 4) ? 0 : Random.Range(0, Platforms.Length);
+
+            var pos = (_lastPlatform == null) 
+                ? Platforms[0].transform.position : _lastPlatform.transform.position;
             var zPosition = (zPos == -1) 
-                ? _lastPlatform.transform.position.z + _platformLength 
-                : zPos;
+                ? _lastPlatform.transform.position.z + _platformLength : zPos;
+
+            // -0.02f is to compensate for rigidbody collision with floor failure
             var platform = Instantiate(
-                Platform,
-                new Vector3(pos.x, pos.y, zPosition),
+                Platforms[platformNumber],
+                new Vector3(pos.x, pos.y - 0.02f, zPosition),
                 Quaternion.identity) as GameObject;
 
             platform.transform.parent = transform;
 
-            var world = GameManager.Instance.Platforms;
+            
             world.Enqueue(platform);
 
-            //if (removeOld) Destroy(world.Dequeue()); // TODO: pool different platforms
+            if (removeOld) Destroy(world.Dequeue()); // TODO: pool different platforms
 
             _lastPlatform = platform.transform;
 
