c36b611a883d0fcfab27494a389362134891d7d0
Goos van den Bekerom <g.vandenbekerom@student.fontys.nl>
2016-09-23 08:47:39 +0200

    Added primitive random coin spawning

---
+++ Assets/Scripts/CoinPoolScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CoinPoolScript : MonoBehaviour
+    {
+
+        private static CoinPoolScript _instance;
+
+        public static CoinPoolScript Instance
+        {
+            get { return _instance ?? new CoinPoolScript(); }
+        }
+
+        public GameObject CoinPrefab;
+        public Transform Player;
+
+        private const int CoinPoolSize = 50;
+        private Queue<Transform> _coins;
+
+        private float _lastCoinYpos;
+        private const float YScale = 0.0025f;
+
+        private int _lastCoinZpos;
+
+        private const int CoinStartPos = 10;
+        private const int CoinsInRow = 20; // 10*2 for spacing between coins
+        private int _currentXspawnPos;
+
+        void Awake()
+        {
+            _instance = this;
+            _lastCoinYpos = 1;
+        }
+
+        void Start()
+        {
+            _coins = new Queue<Transform>(CoinPoolSize);
+            _currentXspawnPos = 0;
+            _lastCoinZpos = CoinStartPos;
+
+            for (var i = CoinStartPos; i < CoinPoolSize+ CoinStartPos; i++)
+            {
+                var coin = Instantiate(CoinPrefab, new Vector3(_currentXspawnPos, _lastCoinYpos, _lastCoinZpos), Quaternion.identity) as GameObject;
+                coin.transform.parent = transform;
+                _coins.Enqueue(coin.transform);
+                _lastCoinYpos -= YScale;
+                 _lastCoinZpos += 2;
+
+                if (i == 10) continue; //so the first coin isn't off
+
+                RandomizeCoinPosition();
+            }
+        }
+
+        public void RandomizeCoinPosition()
+        {
+            if ((_lastCoinZpos%CoinsInRow) != 0) return;
+
+            _lastCoinZpos += 4; // to give player time to change rows
+            _currentXspawnPos = Random.Range(-2, 3);
+        }
+
+        public void ReplaceCoin()
+        {
+            var coinPos = _coins.Peek().position;
+            if (coinPos.y < 15 && coinPos.z > (Player.position.z - 2)) return;
+            //coin has been missed or picked up.
+
+            var coin = _coins.Dequeue();
+            _lastCoinYpos -= YScale;
+            _lastCoinZpos += 2;
+            coin.position = new Vector3(_currentXspawnPos, _lastCoinYpos, _lastCoinZpos);
+            _coins.Enqueue(coin);
+
+            RandomizeCoinPosition();
+        }
+    }
+}
---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CoinScript : MonoBehaviour
+    {
+        public int Value;
+
+        void Start()
+        {
+            if (Value == 0) Value = 1;
+        }
+
+        void Update()
+        {
+            if (transform.position.z < (CoinPoolScript.Instance.Player.position.z - 2))
+            {
+                CoinPoolScript.Instance.ReplaceCoin();
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                ScoreManager.Instance.IncreaseCoins(Value);
+                transform.Translate(Vector3.up*20);
+                CoinPoolScript.Instance.ReplaceCoin();
+            }
+        }
+    }
+}
